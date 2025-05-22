6cddecc6f79c639623b6797fce701b71ff4019d5
Vrushali More <132755812+vrushmore123@users.noreply.github.com>
2025-05-23 01:01:29 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,17 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(),  tailwindcss()],
+  plugins: [react(), tailwindcss()],
+  server: {
+    host: '0.0.0.0', // Required for Render to detect the port
+    port: 5173,       // Optional: default Vite port
+    proxy: {
+      '/api': {
+        target: 'http://localhost:8000', // Your FastAPI backend URL
+        changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/api/, ''),
+      },
+    },
+  },
 })
